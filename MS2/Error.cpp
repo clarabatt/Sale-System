69f12a2124d5fc69a7da607d14c038e6f07c1142
Clara Battesini <clarabattesini@gmail.com>
2023-03-18 13:25:56 -0400

    changes on error class

--- MS2/Error.cpp
+++ MS2/Error.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
+#include <cstring>
 #include "Error.h"
 
 using namespace std;
 
 namespace sdds {
 
+    Error::Error(){
+        error_message = nullptr;
+    };
+
     Error::Error(const char* msg){
         if (msg != nullptr and msg[0] != '\0'){
-            error_message = new char [len(msg+1)];
+            error_message = new char [strlen(msg)+1];
             strcpy(error_message, msg);
         }
     };
     Error::Error(Error& copyError){
         if (copyError.error_message != nullptr and copyError.error_message[0] != '\0'){
-            error_message = new char [len(msg+1)];
+            error_message = new char [strlen(copyError.error_message)+1];
             strcpy(error_message, copyError.error_message);
-            delete [] copyError.error_message;
-            copyError.error_message = nullptr;
+            copyError.clear();
         }
     };
     Error::~Error(){
@@ -25,11 +29,46 @@ namespace sdds {
     };
     Error& Error::operator=(Error& copyError){
         if (copyError.error_message != nullptr and copyError.error_message[0] != '\0'){
-            error_message = new char [len(msg+1)];
+            error_message = new char [strlen(copyError.error_message)+1];
             strcpy(error_message, copyError.error_message);
-            delete [] copyError.error_message;
-            copyError.error_message = nullptr;
+            copyError.clear();
         }
+        return *this;
     };
+
+    Error& Error::clear(){
+        delete [] error_message;
+        error_message = nullptr;
+        return *this;
+    };
+
+    Error::operator bool() const{
+        return error_message != nullptr and error_message[0] != '\0';
+    };
+
+    char* Error::getMessage() const{
+        return error_message;
+    };
+
+    Error& Error::operator=(const char* msg){
+        if (msg == nullptr || *msg == '\0') {
+            clear();
+        } else {
+            if (error_message != nullptr) {
+                delete[] error_message;
+            }
+            int len = strlen(msg);
+            error_message = new char[len + 1];
+            strcpy(error_message, msg);
+        }
+        return *this;
+    };
+
+    ostream& operator<<(ostream& ostr, const Error& right){
+        ostr << right.getMessage();
+        return ostr;
+    };
+
+
 
 }
--- MS2/Error.h
+++ MS2/Error.h
@@ -12,19 +12,31 @@
 #ifndef SDDS_ERROR_H_
 #define SDDS_ERROR_H_
 
+#include <iostream>
+
+using namespace std;
+
 namespace sdds {
 
     class Error {
         char* error_message = nullptr;
 
     public:
+        Error();
         Error(const char* msg);
-        Error(Error& newError);
+        Error(Error& copyError);
         ~Error();
-        Error& operator=(Error& newError);
+        Error& operator=(Error& copyError);
+        operator bool() const;
+        char* getMessage() const;
+        Error& clear();
+        Error& operator=(const char* msg);
 
     };
 
+    ostream& operator<<(ostream& ostr, const Error& right);
+
 }
+
 #endif // !SDDS_ERROR_H_
 
